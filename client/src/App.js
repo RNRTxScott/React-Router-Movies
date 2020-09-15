a48ad02922245f732ad05d3ea3d660f4e8b68b25
Scott Vanderburg <s.vanderburg@realnerdsrt.com>
2020-09-15 13:26:10 -0600

    mostly complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieCard from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 import SavedList from './Movies/SavedList';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  const [saved, setSaved] = useState([]); 
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies') 
         .then(response => {
+          setMovieList(response.movie.id)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -23,14 +27,16 @@ export default function App () {
   }, []);
 
   const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
+    
   };
 
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div><Route exact path ='/movielist'  />
+      <Route exact path = '/movielist/movies/' /> 
+      <MovieList /></div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-// You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+// You'll need to wrap <App /> for routing to work - done
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
